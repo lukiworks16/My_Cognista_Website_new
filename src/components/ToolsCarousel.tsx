9b54f34b5ecb98da4c435f99b5e3372f596993f6
lukiworks16 <luki.works16@gmail.com>
2025-08-26 00:27:45 +0530

    Update ToolsCarousel.tsx remove tech cat

--- src/components/ToolsCarousel.tsx
+++ src/components/ToolsCarousel.tsx
@@ -249,7 +249,7 @@ const ToolsCarousel: React.FC = () => {
         </div>
 
         {/* Technology Categories */}
-        <div className="mt-16">
+{/*         <div className="mt-16">
           <h3 className="text-2xl font-bold text-center mb-8 text-white">Technology Categories</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {[
@@ -271,7 +271,7 @@ const ToolsCarousel: React.FC = () => {
               </div>
             ))}
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
